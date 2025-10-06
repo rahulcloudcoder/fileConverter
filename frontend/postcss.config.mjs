fbda75d275c9d1f4c7ac4b3db3fb30b439a6d918
rahulcloudcoder <your.email@example.com>
2025-10-06 19:28:43 +0530

    dfe

--- frontend/postcss.config.mjs
+++ frontend/postcss.config.mjs
@@ -1,9 +1,5 @@
-/** @type {import('postcss').ProcessOptions} */
 const config = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  plugins: ["@tailwindcss/postcss"],
 };
 
 export default config;
